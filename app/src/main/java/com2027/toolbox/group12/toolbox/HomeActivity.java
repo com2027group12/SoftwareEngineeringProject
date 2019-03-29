a39c23ee148d7af529b5605a84033e537d867f35
Nik <nn00317@surrey.ac.uk>
2019-03-29 16:29:30 +0000

    Added Project Structure
    Added Project Structure

---
+++ app/src/main/java/com2027/toolbox/group12/toolbox/HomeActivity.java
@@ -0,0 +1,13 @@
+package com2027.toolbox.group12.toolbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
